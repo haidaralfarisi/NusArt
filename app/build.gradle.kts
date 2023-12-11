765dc8851b2017fe54a60c042c8b66fe95cc974a
haidaralfarisi <haidaralfarisi24@gmail.com>
2023-12-11 22:47:07 +0700

    register activity, login actiivty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation ("com.google.android.material:material:1.3.1")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
